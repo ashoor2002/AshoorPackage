413b3ee4b1adb751b90c40ee0767c8933dd93981
ABDUL <ashoor2002@gmail.com>
2018-01-11 20:31:12 -0800

    my first Package

---
+++ R/ashoor-functions.R
@@ -0,0 +1,17 @@
+num_count <- function(num, vect){
+    count <- 0
+    for(x in vect){
+        if(x == num){
+            count <- count +1
+
+
+        }
+    }
+    return(count)
+}
+
+
+ashoor.function <- function(){
+    print("Hi, I am from Ashoor Package")
+}
+
